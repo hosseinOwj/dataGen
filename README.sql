8e19dfd3f8d974dad5269fb88e7beb0e997bc2fb
Mohammad Hossein Owj <owj.hossein@gmail.com>
2016-09-28 17:12:57 -0600

    Update README.sql

--- README.sql
+++ README.sql
@@ -3,7 +3,7 @@
 **********************************/
 -- ABOUT:
 
--- This project is designed to generate sample data for  Credit Card Company, CPI Card Group, Inc., data warehouse.
+-- This project is designed to generate sample data for A Credit Card Company, CPI Card Group, Inc., data warehouse.
 -- This software generates data for different tables of data warehouse including:
 -- DIMENTION TABLES: W_TIME_D, W_SALES_AGENT_D, W_CUSTOMER_D, W_CUST_LOCATION_D
 -- FACT TABLES: W_LEAD_F, W_JOB_F, W_SUB_JOB_F, W_JOB_SJIPMENT_F, W_INVOICELINE_F, W_FINANCIAL_SUMMARY_SALES_F, W_FINANCIAL_SUMMARY_COST_F
@@ -20,5 +20,6 @@
 --***NOTICE*** if you want to manually configure the software, edit and run 'configuration.sql' file in the main directory.
 --3. Using the command help populate data for non-hard coded dimentions including: W_TIME_D, W_SALES_AGENT_D, W_CUSTOMER_D, W_CUST_LOCATION_D
 --4. Using the command help populate data for fact tables. 
+--5. You need have a user able to create/remove tables, functions, procedures,sequences and indexes
 -- ***NOTICE*** There are some execution sample files in '\sample' folder for noth Fact and Dimention data generation. 
 -- ***NOTICE*** You might need to specify some indexes for user specified executions. 
